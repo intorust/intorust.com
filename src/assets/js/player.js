fb7200326e55aaf8e08644c91c7e4ec07fc959de
Niko Matsakis <niko@alum.mit.edu>
2016-09-30 08:38:55 -0400

    various adjustments to layout

--- src/assets/js/player.js
+++ src/assets/js/player.js
@@ -26,7 +26,6 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes.
