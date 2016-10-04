48abd11324c21c358ad9d26393847bf0092246c3
Ernest S <c64@iest.pl>
2016-10-04 17:06:43 +0200

    Add PROMPT and SOLUTION transforms.

--- src/assets/js/play_url.js
+++ src/assets/js/play_url.js
@@ -37,8 +37,26 @@
   }
 
   function addTransformations(text) {
-    text = text.replace("http://home.url/", HOME_URL);
-    text = text.replace(/\/\/\s*PROMPT\s*(.*)/, "$1");
+    var solutionRegex = /\/\/ START SOLUTION\s*(.*)\s*\/\/ END SOLUTION/g;
+    var promptRegex = /\/\/\s*PROMPT\s*(.*)/g
+
+    text = text.replace("http://home.url/g", HOME_URL);
+    if (/PROMPT/.test(text)) {
+      if (promptRegex.test(text)) {
+        text = text.replace(promptRegex, "$1");
+      } else {
+        console.error("PROMPT keyword found but the actual match failed");
+      }
+    }
+
+    if (/START|END|SOLUTION/.test(text)) {
+      if (solutionRegex.test(text)) {
+        text = text.replace(solutionRegex, "");
+      } else {
+        console.error("START|END|SOLUTION keyword(s) found but the actual match failed");
+      }
+    }
+
     return text;
   }
 
