97c7b793feec1417d0da8c140a66ad9e7e15bc8a
Ernest S <c64@iest.pl>
2016-10-04 15:11:56 +0200

    Various cleanups and refactorings.

--- src/assets/js/play_url.js
+++ src/assets/js/play_url.js
@@ -1,43 +1,42 @@
-// fetch exercises even before user clicks on any link!
-(function(window, document) {
+(function (document) {
   "use strict";
 
-  function get(path) {
-    var basePath = "/exercises/";
+  var EXERCISES_PATH = "/exercises";
+  var PLAY_HOST = "https://play.rust-lang.org";
+  var RUST_VERSION = "nightly";
+
+  document.addEventListener("DOMContentLoaded", function() {
+    playUrl();
+  });
+
+  function playUrl() {
+    var exerciseLinks = document.querySelectorAll("[data-exercise-uri]");
+
+    exerciseLinks.forEach(function(link) {
+      var exerciseUri = link.getAttribute("data-exercise-uri");
+      fetchExercise(exerciseUri, function(code) {
+        // Replace link's href with Play URL:
+        link.href = PLAY_HOST + "?version=" + RUST_VERSION +
+          "&code=" + encodeURIComponent(addTransformations(code));
+        // Remove class giving a visual cue, that something's changed:
+        link.className = link.className.replace(/exercise-link-loading/, "");
+      });
+    });
+  }
+
+  function fetchExercise(path, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open("get", basePath + path, true);
+    xhr.open("get", EXERCISES_PATH + "/" + path, true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-        sendToPlay(xhr.responseText);
+        callback(xhr.responseText);
       }
     };
     xhr.send();
   }
 
-  function sendToPlay(code) {
-    var uri = "https://play.rust-lang.org?version=nightly";
-    uri += "&code=" + encodeURIComponent(code);
-    window.open(uri, "_blank");
+  function addTransformations(text) {
+    return text;
   }
 
-  window.playUrl = function playUrl(msg) {
-    console.log(msg);
-    get(msg);
-    // var exerciseLinks = document.querySelectorAll('[data-exercise-uri]');
-    //     var exerciseUri = ev.target.getAttribute('data-exercise-uri');
-    //     get(exerciseUri);
-
-    // exerciseLinks.forEach(function(link, i) {
-    //   link.addEventListener("click", function(ev) {
-    //     ev.preventDefault();
-    //     var exerciseUri = ev.target.getAttribute('data-exercise-uri');
-    //     get(exerciseUri);
-    //   });
-    // });
-  }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    // playUrl();
-  });
-
-})(window, document);
+})(document);
